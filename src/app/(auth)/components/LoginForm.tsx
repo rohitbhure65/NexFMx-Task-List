a2a26ac44080cc016afad60102d0d3acdfdad866
Rohit Bhure <rohitbhure.cse@gmail.com>
2024-11-01 12:13:36 +0530

    check point 3

--- src/app/(auth)/components/LoginForm.tsx
+++ src/app/(auth)/components/LoginForm.tsx
@@ -37,6 +37,7 @@ export const LoginForm = (props: LoginFormProps) => {
             } else {
               router.push("/")
             }
+            router.push(`/tasks`)
           } catch (error: any) {
             if (error instanceof AuthenticationError) {
               return { [FORM_ERROR]: "Sorry, those credentials are invalid" }
